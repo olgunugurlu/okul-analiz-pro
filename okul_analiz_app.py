5dc8401f4a61528e12906cf19fca2156522ef2d2
Olgun Uğurlu <olgunugurlu@gmail.com>
2026-01-13 11:06:32 +0300

    Kodlar güncellendi, streamlit embed removed.

--- okul_analiz_app.py
+++ okul_analiz_app.py
@@ -30,10 +30,10 @@ except Exception:
 #     st.query_params["embed"] = "true"
 #     st.rerun()
 
-params = dict(st.query_params)
-if params.get("embed") != "true":
-    st.info("Daha temiz görünüm için uygulamayı Embed modda açabilirsiniz.")
-    st.link_button("Embed modda aç", "?embed=true")
+# params = dict(st.query_params)
+# if params.get("embed") != "true":
+#     st.info("Daha temiz görünüm için uygulamayı Embed modda açabilirsiniz.")
+#     st.link_button("Embed modda aç", "?embed=true")
 
 # -------------------- SAYFA AYARLARI --------------------
 st.set_page_config(
